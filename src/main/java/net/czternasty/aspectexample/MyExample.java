64a63e15c70fa50516d8fef94772341e169ede1f
Rémi Czternasty <remi.czternasty@7circles.com>
2017-10-21 11:43:05 +0200

    [master] Initial commit

---
+++ src/main/java/net/czternasty/aspectexample/MyExample.java
@@ -0,0 +1,8 @@
+package net.czternasty.aspectexample;
+
+import net.czternasty.aspectroot.HasTitle;
+import net.czternasty.aspectroot.IGhostable;
+
+public class MyExample implements IGhostable, HasTitle {
+
+}
