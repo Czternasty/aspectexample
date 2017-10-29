40968ae1bd14cd2f3d4cf9ee7d25a440e029003d
Rémi Czternasty <remi.czternasty@7circles.com>
2017-10-29 18:23:30 +0100

    [master] Source directory and clean

--- src/net/czternasty/aspectexample/MyExample.java
+++ src/net/czternasty/aspectexample/MyExample.java
@@ -1,8 +1,7 @@
 package net.czternasty.aspectexample;
 
-import net.czternasty.aspectroot.HasTitle;
 import net.czternasty.aspectroot.IGhostable;
 
-public class MyExample implements IGhostable, HasTitle {
+public class MyExample implements IGhostable {
 
 }
